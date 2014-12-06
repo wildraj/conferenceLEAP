ec79627428ac6be7e8800be372aa5f828c5ecddf
wildraj <rsp10a@acu.edu>
2014-12-06 13:42:47 -0600

    Log-in change(take out function)

--- user/log.php
+++ user/log.php
@@ -1,16 +1,38 @@
 <?php
+	session_start();	
 	require_once('DBQuery.php');
-	function logInValidation($email, $pw){
+	//This function searches for correct username and password, if found, then true; if not then it's invalid
+	
 		//This query searches for the requested username with the requested password
-		$query = "SELECT * FROM user WHERE Username='".$email."' AND password='".$pw."';";
-		$validation = new DBQuery($query);
-		$validation->execute_query();
-		//Search for correct username with the correct password
-		$valid = $validation->get_result();
-		if ($valid->num_rows != 1){
-			return false;
+		
+		$email = $_POST['email'];
+		$password = md5($_POST['password']);
+		$query = "SELECT * FROM user WHERE `email`= '".$email."' AND `password`= '".$password."';";
+		$valid = new DBQuery($query);
+		$valid->execute_query();
+		$result = $valid->get_result();
+		$num_results = $result->num_rows;
+		$valid->close(); 
+		if($num_results != 1){
+			$_SESSION['invalid'] = 'Incorrect name or password<br>';
+			//header("Location: log in page");
 		}
-		//GET ALL USER INFORMATION?
-		return true;
+		else{
+			$row = $result->fetch_assoc();
+			//user's role, email, and id
+			$_SESSION['role']=$row['role'];
+			$_SESSION['email']=$row['username'];
+			$_SESSION['user_id']=$row['id'];
+			
+			//header("Location: main.php");
+		}
+		//PUT THIS IN THE MIDDLE OF THE INDEX PAGE FOR LOGI-IN VALIDATION
+		//start_session() at the top of page
+		/*if (isset($_SESSION['invalid'])){
+			echo "<p style='color:red; margin:5px'>".$_SESSION['invalid']."</p><br>";
+			unset($_SESSION['invalid']);
+		}*/
+		
 	}
+	
 ?>
